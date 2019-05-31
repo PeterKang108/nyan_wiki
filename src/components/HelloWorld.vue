4c0bbde54e2933745c72cdd8ba6f24d744f8e1cd
PeterKang108 <Peterkang7000@outlook.com>
2019-05-31 11:29:53 +0800

    random test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="homepage" id="homepage">
+    <h1>{{ mainTitle }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      This is the homepage of nyan_wiki
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Why my mainTitle can not be loaded?</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
@@ -27,22 +27,42 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <ul>
+      <label>
+        <input type=text v-model="testInput" placeholder="怎么这么麻烦">
+      </label>
+    </ul>
+    <h4>{{testInput}}</h4>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'nyan_wiki',
+  data() {
+    return{
+      mainTitle:"Nyan Wiki",
+      testInput:"",
+    }
   }
+
 }
+
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+h1 {
+  margin: 40px 0 0;
+}
 h3 {
   margin: 40px 0 0;
+}
+h4 {
+  margin: 10px 0 0;
 }
 ul {
   list-style-type: none;
